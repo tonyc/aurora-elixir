a50609d1f712681006019f0c94f149ce63a9562e
Tony Collen <tony@halogenlabs.com>
2015-05-30 15:45:15 -0500

    initial import

---
+++ parse-data.exs
@@ -0,0 +1,32 @@
+defmodule Errors do
+  def generic_error(filename, reason) do
+    IO.puts "Error reading '#{filename}': #{reason}"
+  end
+end
+
+defmodule DataParser do
+  def read_file(file) do
+    IO.puts "Read file"
+
+    String.split(file, "\n")
+    |> Enum.filter(fn(line) -> !String.match?(line, ~r/[:#]/) end)
+    |> Enum.map(&line_to_record/1)
+    |> Enum.each(&IO.puts/1)
+  end
+
+  def line_to_record(line) do
+    # 2014 11 24  2223   56985   80580    0    -4.5     2.4     1.6     5.3    17.1   151.4
+    #{year, month, day, time, julian_day, day_seconds, s, bx, by, bz, bt, lat, lon} = String.split(line, ~r/\ +/)
+    String.split(line, ~r/\ +/)
+
+
+  end
+
+end
+
+filename = "sample_data/ace_mag_1m.txt"
+
+case File.read(filename) do
+  {:ok, file} -> DataParser.read_file(file)
+  {:error, reason} -> Errors.generic_error(filename, reason)
+end
